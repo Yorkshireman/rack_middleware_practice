6ec09050cc217972eb4b52a144f70262054d2d51
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-05-04 00:34:59 +0800

    demonstrate Rack::Response long-hand

--- message_app.rb
+++ message_app.rb
@@ -1,5 +1,7 @@
+require 'rack'
 class MessageApp
   def call(env)
-    [200, {}, [env['MESSAGE']]]
+    # [200, {}, [env['MESSAGE']]]
+    Rack::Response.new([env['MESSAGE']], 200, {}) #or could omit the second two args?
   end
 end
